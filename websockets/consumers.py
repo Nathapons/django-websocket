5a15a141864e1ecc04f6536f44d57773bd1be71a
Nuthapon Sripornprasert <nuthapon@swiftdynamics.co.th>
2023-11-16 15:23:10 +0700

    feat: connect and disconnect chat

--- websockets/consumers.py
+++ websockets/consumers.py
@@ -1,15 +1,23 @@
-import json
+import re
+
+from asgiref.sync import async_to_sync
 
 from channels.generic.websocket import JsonWebsocketConsumer
 
+
 class MyConsumer(JsonWebsocketConsumer):
+    group_name = ''
 
     def connect(self):
         # Will work when socket connect
         self.user = self.scope['user']
         if not self.user.is_anonymous:
+            email = re.sub("[@\.+]", "_", self.user.email)
+            self.group_name = f'chat_{email}'
+            async_to_sync(self.channel_layer.group_add)(
+                self.group_name, self.channel_name
+            )
             self.accept("Bearer")
-            self.send_json({self.user.id: 'Connect'})
         else:
             self.close()
 
@@ -17,3 +25,9 @@ class MyConsumer(JsonWebsocketConsumer):
         # Will work when socket send message
         user = self.scope['user']
         self.send_json({user.id: "Receive"})
+
+    def disconnect(self, code):
+        if self.group_name:
+            async_to_sync(self.channel_layer.group_discard)(
+                    self.group_name, self.channel_name
+            )
