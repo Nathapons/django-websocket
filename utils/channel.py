134ffcb0a79acc0dbc7d5881079549aa5ec88471
Nuthapon Sripornprasert <nuthapon@swiftdynamics.co.th>
2023-11-16 13:55:44 +0700

    refact: remove unused code

--- utils/channel.py
+++ utils/channel.py
@@ -24,22 +24,6 @@ def get_user_by_access_token(scope):
         raise ValueError("Cannot find access_token in header.")
 
 
-class CustomOauth2TokenMiddleware(AuthMiddleware):
-
-    def populate_scope(self, scope):
-        authen_header = list(filter(lambda header: header[0].decode('utf-8') == 'authorization', scope["headers"]))
-        if authen_header:
-            token = authen_header[0][1].decode('utf-8')
-            token = base64.b64decode(token[6:]).decode('utf-8')[:-1]
-            scope['access_token'] = token
-
-        if "user" not in scope:
-            scope["user"] = UserLazyObject()
-    
-    async def resolve_scope(self, scope):
-        scope["user"]._wrapped = await get_user_by_access_token(scope)
-
-
 class CustomOauth2SubProtocolMiddleware(AuthMiddleware):
 
     def populate_scope(self, scope):
@@ -51,11 +35,6 @@ class CustomOauth2SubProtocolMiddleware(AuthMiddleware):
 
     async def resolve_scope(self, scope):
         scope["user"]._wrapped = await get_user_by_access_token(scope)
-
-
-def CustomAuthTokenMiddlewareStack(inner):
-    return CookieMiddleware(SessionMiddleware(CustomOauth2TokenMiddleware(inner)))
-
 
 def CustomAuthSubProtocolMiddlewareStack(inner):
     return CookieMiddleware(SessionMiddleware(CustomOauth2SubProtocolMiddleware(inner)))
