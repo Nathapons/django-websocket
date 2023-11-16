64a3d0213a1438ea51feb31f9fa705a459d9e05f
Nuthapon Sripornprasert <nuthapon@swiftdynamics.co.th>
2023-11-16 14:46:36 +0700

    feat: send_json message

--- websockets/consumers.py
+++ websockets/consumers.py
@@ -1,18 +1,19 @@
 import json
-from channels.generic.websocket import WebsocketConsumer
 
-class MyConsumer(WebsocketConsumer):
+from channels.generic.websocket import JsonWebsocketConsumer
+
+class MyConsumer(JsonWebsocketConsumer):
 
     def connect(self):
+        # Will work when socket connect
+        self.user = self.scope['user']
+        if not self.user.is_anonymous:
+            self.accept("Bearer")
+            self.send_json({self.user.id: 'Connect'})
+        else:
+            self.close()
+
+    def receive_json(self, content, **kwargs):
+        # Will work when socket send message
         user = self.scope['user']
-        print(f'{user.__dict__ = }')
-        self.accept()
-
-    def receive(self, text_data):
-        # You can process the received message here
-        text_data_json = json.loads(text_data)
-
-        # Echo the message back to the client
-        self.send(text_data=json.dumps({
-            'message': text_data_json['message']
-        }))
+        self.send_json({user.id: "Receive"})
